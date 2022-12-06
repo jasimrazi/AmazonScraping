a8fbc269dec09381889ef75ef481cd7b9446df7a
jasimrazi <jasimrazi17@gmail.com>
2022-12-06 11:24:13 +0530

    Third Commit

--- amazon.py
+++ amazon.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.amazon.in/s?k=laptop&crid=2XAXZO02VK979&sprefix=laptop%2Caps%2C275&ref=nb_sb_noss_2"
 
@@ -10,36 +11,33 @@ soup = BeautifulSoup(html_text, 'lxml')
 results = soup.find_all('div', {'data-component-type': 's-search-result'})
 
 
-
 def scrape():
     atag = item.h2.a
     name = atag.text
-    link = 'https://www.amazon.in'+atag.get('href')
-    price_parent = item.find('span','a-price')
+    # link = 'https://www.amazon.in'+atag.get('href')
+    price_parent = item.find('span', 'a-price')
     price = price_parent.find('span', 'a-offscreen').text
     price_before_parent = item.find('span', class_='a-price a-text-price')
     price_before = price_before_parent.find('span', class_='a-offscreen').text
     rating = item.i.text
-    try: 
-        delivery_parent = item.find('div', class_ = "a-row s-align-children-center")
-        delivery = delivery_parent.find('span',class_ = 'a-color-base puis-medium-weight-text').text
+    producturl ='https://www.amazon.in' + atag.get('href')
+    try:
+        delivery_parent = item.find(
+            'div', class_="a-row s-align-children-center")
+        delivery = delivery_parent.find(
+            'span', class_='a-color-base puis-medium-weight-text').text
     except AttributeError:
         delivery = ' '
 
     # result = (name, price, price_before, rating, delivery)
-    result_title = ('Name', 'Price', 'Price Before', 'Rating', 'Delivery')
-    result_content = (name, price, price_before, rating, delivery)
+    # result_title = ('Name', 'Price', 'Price Before', 'Rating', 'Delivery')
+    # result_content = (name, price, price_before, rating, delivery)
 
-    result = dict(zip(result_title, result_content))
+    result = (name, price, price_before, rating, delivery, producturl)
+    print(result)
 
-    return result
+    return 
+
 
 for item in results:
-    x = x.append (scrape())
-    
-    
-
-output = json.loads(x)
-print(output)
-
-
+    scrape()
